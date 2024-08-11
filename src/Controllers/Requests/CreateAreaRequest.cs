28fbad43f63ebf105d4047e70e914f92a155bbfc
t <x@y.com>
2024-08-11 18:46:19 +0100

    adding area model, request and repository interface

---
+++ src/Controllers/Requests/CreateAreaRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WritersBlockAPI.Controllers.Requests
+{
+    public class CreateAreaRequest
+    {
+        public int LocationId { get; set; }
+        public int AreaTypeId { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+    }
+}
---
+++ src/Repositories/IAreaRepository.cs
@@ -0,0 +1,14 @@
+﻿using WritersBlockAPI.Controllers.Requests;
+using WritersBlockAPI.Models;
+
+namespace WritersBlockAPI.Repositories
+{
+    public interface IAreaRepository
+    {
+        void Create(CreateAreaRequest createAreaRequest);
+        List<Area> All();
+        List<Area> All(int locationId);
+        Area Find(int areaId);
+        void Destroy(int areaId);
+    }
+}
